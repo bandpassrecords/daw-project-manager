b2a0539bd4a01d977dbb296ef7d0150594a8711a
Gabriel Becker <gabrielgasparbecker@gmail.com>
2025-12-23 03:30:42 +0100

    bump the version

--- lib/ui/dashboard_page.dart
+++ lib/ui/dashboard_page.dart
@@ -26,7 +26,7 @@ import '../providers/providers.dart';
 import '../repository/project_repository.dart';
 import 'package:uuid/uuid.dart';
 
-const String kAppVersion = '1.4.0';
+const String kAppVersion = '1.5.0';
 
 // WIDGET CORRIGIDO: Botões de controle da janela usando window_manager
 class WindowButtons extends StatelessWidget {
